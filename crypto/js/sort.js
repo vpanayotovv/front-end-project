a0bb6ed6e8ce6414e0162805e21f367ccd5b77da
Vasil <vvpanayotovv@gmail.com>
2021-05-12 14:47:18 +0300

    implement coin page and sort.js

---
+++ crypto/js/sort.js
@@ -0,0 +1,33 @@
+function sortTable(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    const sortedRows = rows.sort((a, b) => {
+
+        const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+
+    });
+
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    tBody.append(...sortedRows);
+
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("ths-sort-desc", !asc);
+}
+
+document.querySelectorAll(".sortable th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currAsc = headerCell.classList.contains("th-sort-asc");
+
+        sortTable(tableElement, headerIndex, !currAsc)
+    });
+});
