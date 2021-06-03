f408488971e4e8912dcc074475993258c7686f27
Vasil <vvpanayotovv@gmail.com>
2021-06-03 20:09:37 +0300

    implementing api

--- crypto/js/api.js
+++ crypto/js/api.js
@@ -1,6 +1,8 @@
+let rates = {};
+
 (function ($) {
     var settings = {
-        "url": "http://rest.coinapi.io/v1/assets?filter_asset_id=BTC,ETH,ADA",
+        "url": "http://rest.coinapi.io/v1/assets?filter_asset_id=BTC,ETH,ADA,BNB,DOGE,USDT,XRP,ICP,DOT,BCH,UNI,LTC,LINK,XLM,USDC",
         "method": "GET",
         "timeout": 0,
         "headers": {
@@ -10,17 +12,37 @@
 
     $.ajax(settings).done(function (response) {
         response.forEach(element => {
-            //TODO:add rates in object!
+            let name = element.asset_id;
+            let price = element.price_usd;
+            rates[name] = price;
         });
     });
 }(jQuery));
 
-// the array of elements with prices
+// let rates2 = {
+//     BNB: 267.512316,
+//     ETH: 2286.3321321,
+//     LTC: 43.0213216,
+//     BTC: 43872.06,
+//     USDT: 1.00231321,
+//     DOGE: 0.513123213,
+//     XRP: 1.48,
+//     ICP: 299.821316,
+//     ADA: 1.78,
+//     BCH: 1547.152131,
+//     LINK: 48.3213219,
+//     DOT: 10.64213,
+//     USDC: 1.00213213,
+//     XLM: 0.21321321,
+//     UNI: 381.62132138,
+// };
 
-let priceElements = document.querySelectorAll("body > table > tbody > tr > td:nth-child(3)");
+let priceElements = document.querySelectorAll("body > table > tbody > tr > td:nth-child(3) > span");
+
 for (let i = 0; i < priceElements.length; i++) {
-    let element = priceElements[i];
-    let price = element.innerHTML.split('>')[2];
+    let price = priceElements[i];
+    let nameElement = price.parentElement.parentElement.children[1].textContent;
+    let nameArray = nameElement.split(' ');
+    let name = nameArray[nameArray.length - 1];
+    price.textContent = rates2[name];
 }
-
-
