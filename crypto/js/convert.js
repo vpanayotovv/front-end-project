bc3f0576a1b4d67904bd7d940270a51c9514598a
Vasil <vvpanayotovv@gmail.com>
2021-06-08 02:58:20 +0300

    add button exchange

--- crypto/js/convert.js
+++ crypto/js/convert.js
@@ -1,12 +1,12 @@
 const btnElement = document.getElementById('cBtn');
 
 btnElement.addEventListener('click', () => {
-    const base = document.getElementById('Base').value;
-    const out = document.getElementById('Output').value;
-    const amount = document.getElementById('Amount').value;
-
+    let base = document.getElementById('Base').value;
+    let out = document.getElementById('Output').value;
+    let amount = document.getElementById('Amount').value;
+    
     let exchangeRate = 0;
-
+    
     $.ajax({
         type: "GET",
         url: `http://rest.coinapi.io/v1/exchangerate/${base}/${out}`,
@@ -18,22 +18,32 @@ btnElement.addEventListener('click', () => {
             exchangeRate = response.rate;
             let finalValue = 0;
             finalValue = exchangeRate * Number(amount);
-
+            
             let result = '';
-
+            
             if (Number(amount)) {
-
+                
                 result = `Converted Amount of ${amount} ${base} is ${finalValue.toFixed(2)} ${out}`;
                 document.getElementById('Converted').innerText = result;
-
+                
             } else {
-
+                
                 result = 'Fill all fileds properly';
                 document.getElementById('Converted').innerText = result;
-
+                
             }
         }
     });
 
-
 });
+
+// const exchangeElement = document.getElementById('exchange');
+//     exchangeElement.addEventListener('click', () => {
+//         let fromElement = document.getElementById('Base');
+//         let toElement = document.getElementById('Output');
+//         let temp = fromElement.innerText;
+//         console.log(temp);
+//         fromElement.innerText = toElement.innerText;
+//         console.log(fromElement.innerText   );
+//         toElement.innerText = temp;
+//     });
