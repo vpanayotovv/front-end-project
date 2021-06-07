094eaf69eea276a47185fa68b81e7e3cf372cf76
Vasil <vvpanayotovv@gmail.com>
2021-06-08 02:13:40 +0300

    update convertor

--- crypto/js/convert.js
+++ crypto/js/convert.js
@@ -1,44 +1,39 @@
 const btnElement = document.getElementById('cBtn');
 
-const rates = {
-
-    "BTC": 56872.06,
-    "ETH": 4286.33,
-    "BNB": 667.56,
-    "DOGE": 0.5133,
-    "USDT": 1.00,
-    "ADA": 1.78,
-    "XRP": 1.48,
-    "ICP": 341.86,
-    "DOT": 39.64,
-    "BCH": 1517.15,
-    "UNI": 43.06,
-    "LTC": 381.68,
-    "LINK": 48.39,
-    "XLM": 0.6853,
-    "USDC": 1.00
-
-}
-
 btnElement.addEventListener('click', () => {
     const base = document.getElementById('Base').value;
     const out = document.getElementById('Output').value;
     const amount = document.getElementById('Amount').value;
 
-    let finalValue = rates;
-    finalValue = finalValue[base] * Number(amount);
+    let exchangeRate = 0;
 
-    let result = '';
+    $.ajax({
+        type: "GET",
+        url: `http://rest.coinapi.io/v1/exchangerate/${base}/${out}`,
+        dataType: "json",
+        headers: {
+            "X-CoinAPI-Key": "76EFC1F0-7005-49CE-AADC-BDBF81C568A5"
+        },
+        success: function (response) {
+            exchangeRate = response.rate;
+            let finalValue = 0;
+            finalValue = exchangeRate * Number(amount);
 
-    if (Number(amount)) {
+            let result = '';
 
-        result = `Converted Amount of ${amount} ${base} is ${finalValue} ${out}`;
-        document.getElementById('Converted').innerText = result;
+            if (Number(amount)) {
 
-    }else{
+                result = `Converted Amount of ${amount} ${base} is ${finalValue.toFixed(2)} ${out}`;
+                document.getElementById('Converted').innerText = result;
 
-        result = 'Fill all fileds properly';
-        document.getElementById('Converted').innerText = result;
+            } else {
 
-    }
+                result = 'Fill all fileds properly';
+                document.getElementById('Converted').innerText = result;
+
+            }
+        }
+    });
+
+
 });
